35e2158e00262289dc1a25f30007e7f3368fa87e
TncBogdan <tncbogdan@gmail.com>
2021-01-26 21:48:36 +0200

    added (componentModel to CountryDTOMapper)

--- user.management/src/main/java/ro/agilehub/javacourse/car/hire/user/controller/mapper/CountryDTOMapper.java
+++ user.management/src/main/java/ro/agilehub/javacourse/car/hire/user/controller/mapper/CountryDTOMapper.java
@@ -5,7 +5,7 @@ import org.mapstruct.Mapping;
 import ro.agilehub.javacourse.car.hire.api.model.CountryDTO;
 import ro.agilehub.javacourse.car.hire.user.service.userDomain.CountryDomain;
 
-@Mapper
+@Mapper(componentModel = "spring")
 public interface CountryDTOMapper {
 
     @Mapping(source = "id", target = "id")
